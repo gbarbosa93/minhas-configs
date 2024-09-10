2cad677c0418b22c0ef291b98d428361386c4df6
Gabriel Nóbrega <78391659+gbarbosa93@users.noreply.github.com>
2024-09-10 17:37:07 -0300

    minhas configurações do vscode

---
+++ minhas-configs.js
@@ -0,0 +1,18 @@
+[
+    {
+        "files.autoSave": "afterDelay",
+        "editor.wordWrap": "on",
+        "editor.fontFamily": "JetBrains Mono", // nome da fonte (Necessario ter instalado a fonte)
+        "editor.fontSize": 14, // tamanho da font (Entre 14 e 16 fica TOP)
+        "editor.fontLigatures": true, // Faz a união de alguns simbolos
+        "editor.renderLineHighlight": "gutter", // mostra o quadradinho apenas na linha que tá selecionada
+        "editor.minimap.enabled": false, // Esconde o mini-mapa do lado esquerdo (porém eu gosto)
+        "workbench.editor.labelFormat": "short", // mostra apenas o nome do arquivo e a paste que ele se encontra no guia
+        "workbench.editor.empty.hint": "hidden",
+        "workbench.startupEditor": "newUntitledFile",
+        "workbench.iconTheme": "symbols", // Biblioteca de icons pra deixar os icones mais bem desenhado
+        "workbench.colorTheme": "Min Dark", // Tema do VSCod
+        "workbench.activityBar.location": "hidden", // Esconde a parte lateral que já tem um acesso com Ctrl+Q
+        "explorer.compactFolders": false // Não deixa as pastas agrupadas
+    }
+]
